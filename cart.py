61a92bb713b050a402ae3ea86ec8e748da87c1cd
Newbluewood <nebojsa.simovic@outlook.com>
2025-02-13 11:31:43 +0100

    new branch created, class Cart added and test in main file

---
+++ cart.py
@@ -0,0 +1,23 @@
+class Cart:
+    def __init__(self):
+        self.cart_items = []
+    
+    def add_product_to_cart(self, product, quantity):
+        if len(self.cart_items) > 0: # ispitivnje dali je korpa prazna i da li vec sadrzi proizvod koji dodajemo
+            for item in self.cart_items:
+                if item["product"].name == product.name:
+                    item["quantity"] += quantity # ako sadrzi, samo korigujemo kolicinu
+                    break
+            else:
+                self.cart_items.append({"product":product, "quantity":quantity})
+        else:
+            self.cart_items.append({"product":product, "quantity":quantity})
+        
+    def total_of_products_in_cart(self):
+        total = sum(item["product"]._price * item["quantity"] for item in self.cart_items)
+        return print(f" Total for all products in cart : {total} $ ")
+    
+    def show_product_in_cart(self):
+        for item in self.cart_items:
+            print(f"- Item: {item["product"].name}, price: {item["product"]._price} $, quantity: {item["quantity"]}")
+        
--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from product_manager import ProductManager
 from product import Product
+from cart import Cart
 
+print("\nFaza 3.\n")
+# kreiranje instance ProductManagera
 ProductManager1 = ProductManager()
 
+# hardkodovano kreiranje proizvoda:
 p1_laptop = Product("Laptop", 350, 20)
 p2_TV = Product("TV Samsung", 480, 15)
 
+# hardkodovano dodavanje proizvoda u listu dostupnih proizvoda instance ProductManagera
 ProductManager1.add_product_to_avaliable_product_list(p1_laptop)
 ProductManager1.add_product_to_avaliable_product_list(p2_TV)
 
-
+# pozivanje metode product_info instance Product
 p1_laptop.product_info()
 p2_TV.product_info()
 
+#pozivanje metoda show_avaliable_products i total_of_products instance ProductManager
 ProductManager1.show_avaliable_products()
 ProductManager1.total_of_products()
 
-print("2")
+# pozivanje dodate metode remove_product_from_list instance ProductManager
+print("\nFaza 4.\n")
 ProductManager1.remove_product_from_list("Laptop")
 ProductManager1.show_avaliable_products()
+
+# testiranje dodate klase Cart i nenih metoda
+print("\nFaza 6. \n")
+cart1 = Cart()
+cart1.add_product_to_cart(p1_laptop, 1)
+cart1.add_product_to_cart(p1_laptop, 3)
+cart1.show_product_in_cart()
+cart1.total_of_products_in_cart()
