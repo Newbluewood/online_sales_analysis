681ec785751dcf5b4b28a2099ddb5cbd8f564301
Newbluewood <nebojsa.simovic@outlook.com>
2025-02-13 12:29:08 +0100

    make some changes in main file

--- main.py
+++ main.py
@@ -3,19 +3,8 @@ from product import Product
 
 ProductManager1 = ProductManager()
 
-p1_laptop = Product("Laptop", 350, 20)
-p2_TV = Product("TV Samsung", 480, 15)
+p1_laptop = Product("Laptop", 750, 20)
+p2_TV = Product("TV Samsung 90''", 980, 15)
 
 ProductManager1.add_product_to_avaliable_product_list(p1_laptop)
 ProductManager1.add_product_to_avaliable_product_list(p2_TV)
-
-
-p1_laptop.product_info()
-p2_TV.product_info()
-
-ProductManager1.show_avaliable_products()
-ProductManager1.total_of_products()
-
-print("2")
-ProductManager1.remove_product_from_list("Laptop")
-ProductManager1.show_avaliable_products()
