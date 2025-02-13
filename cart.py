7a913652fc8c3002ba147fdeaf179b02ad454f0c
Newbluewood <nebojsa.simovic@outlook.com>
2025-02-13 12:19:23 +0100

    3 random product added to cart, Cart add method modified

--- cart.py
+++ cart.py
@@ -6,8 +6,13 @@ class Cart:
         if len(self.cart_items) > 0: # ispitivnje dali je korpa prazna i da li vec sadrzi proizvod koji dodajemo
             for item in self.cart_items:
                 if item["product"].name == product.name:
-                    item["quantity"] += quantity # ako sadrzi, samo korigujemo kolicinu
-                    break
+                    if item.quantity < quantity:
+                        print(" Zao mi je, nemamo toliko na stanju !") # ako kolicina prelazi lager
+                        break
+                    else:
+                        item["quantity"] += quantity # ako sadrzi, samo korigujemo kolicinu
+                        item.quantity - quantity
+                        break
             else:
                 self.cart_items.append({"product":product, "quantity":quantity})
         else:
